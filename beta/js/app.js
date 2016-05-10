91d9484ddabca7bbe3228f28f023dfc5c22f239f
owen <owens.kao@gmail.com>
2016-05-11 00:02:06 +0800

    Sync to alex latest code

--- beta/js/app.js
+++ beta/js/app.js
@@ -8,8 +8,29 @@ $(document).ready(function() {
     var actMap = [];
     var nvrList = [];
     var cameraList = [];
+    var timer = null;
+    var timerDuration = 5*60*1000;
 
     // init
+    var c_userName = getCookie('username');
+    var c_login = getCookie('login');
+    if (c_userName != null && c_userName != "" && c_login == "1") {
+        $("#loginWnd").hide();
+        $("#mainWnd").show();
+        startTimer();
+    } else {
+        if (getCookie('remember') == '1') {
+            $("#rememberMe").prop("checked", true);
+            if (c_userName != null && c_userName != "") {
+                $("#inputAccount").val(c_userName);
+            } else {
+                $("#inputAccount").val("");
+            }
+        } else {
+            $("#inputAccount").val("");
+        }
+    }
+
     $("#siteMenu").hide();
 
     // load json files
@@ -52,14 +73,35 @@ $(document).ready(function() {
 		}
 			
         $("#inputPassword").val("");
+        setCookie('login', '1');
+        setCookie('username', inputAccount);
+
+        if ($("#rememberMe").prop("checked") === true) {
+            setCookie('remember', '1');
+        } else {
+            setCookie('remember', '0');
+            $("#inputAccount").val("");
+        }
 
         $("#loginWnd").hide();
         $("#mainWnd").show();
-		getAllInfo();
+		startTimer();
         //adjustSize();
     });
 
     $(".logout").bind("click", function() {
+        stopTimer();
+        setCookie('login', '0');
+        if (getCookie('remember') == '1') {
+            $("#rememberMe").prop("checked", true);
+            if (c_userName != null && c_userName != "") {
+                $("#inputAccount").val(c_userName);
+            } else {
+                $("#inputAccount").val("");
+            }
+        } else {
+            $("#inputAccount").val("");
+        }
         $("#mainWnd").hide();
         $("#loginWnd").show();
     });
@@ -96,14 +138,28 @@ $(document).ready(function() {
         //adjustSize();
     });
 
+    function startTimer() {
+        stopTimer()
+        timer = setInterval(function(){getAllInfo();}, timerDuration);
+        getAllInfo();
+    }
+
+    function stopTimer() {
+        clearInterval(timer);
+        timer = null;
+    }
+
     function getAllInfo() {
         getRainInfo(-1, function (html){
+            $("#rainInfo").empty();
             $("#rainInfo").append(html);
         });
         getWaterInfo(-1, function (html){
+            $("#waterInfo").empty();
             $("#waterInfo").append(html);
         });
         getEyeInfo(-1, function (html){
+            $("#eyeInfo").empty();
             $("#eyeInfo").append(html);
         });
     }
@@ -176,6 +232,7 @@ $(document).ready(function() {
     }
 
     function handleActionList(actList) {
+        console.log(actList);
         var actType;
         for (var i = 0; i < actList.length; i++) {
             handleAction(actList[i].act, actList[i].param, true);
@@ -222,21 +279,18 @@ $(document).ready(function() {
 
                 html = getActivexHtml(nvr, video);
                 videoWindow = window.open('video.html', "Video");
-                videoWindow.activexContent = html;
-                videoWindow.nvr = nvr;
-                videoWindow.video = video;
                 setTimeout(function(){ 
                     videoWindow.activexContent = html;
                     videoWindow.nvr = nvr;
                     videoWindow.video = video;
                     videoWindow.initActiveX();
-                }, 50);
-                setTimeout(function(){ videoWindow.connect(); }, 100);
+                }, 500);
+                setTimeout(function(){ videoWindow.connect(); }, 1000);
 
                 break;
-           case '7':
+            case '7':
                 // 雨量 rainfall
-				if (allin1) {
+                if (allin1) {
                     var id = param[0]["id"];
                     getRainInfo(id, function (html){
                         if (videoWindow) {
@@ -245,18 +299,17 @@ $(document).ready(function() {
                         }
                     });
                 } 
-				else {
-					var info = {};
-					info['id'] = param[0].id;
-					infoWindow =  window.open('rainfall.html', "info");
-					
-					setTimeout(function(){ infoWindow.getInfo(info); }, 200);	
-				}
-				
+                else {
+                    var info = {};
+                    info['id'] = param[0].id;
+                    infoWindow =  window.open('rainfall.html', "info");
+                    
+                    setTimeout(function(){ infoWindow.getInfo(info); }, 200);   
+                }
                 break;
             case '8':
                 // 水位 waterLevel
-				if (allin1) {
+                if (allin1) {
                     var id = param[0]["id"];
                     getWaterInfo(id, function (html){
                         if (videoWindow) {
@@ -265,22 +318,22 @@ $(document).ready(function() {
                         }
                     });
                 }
-				else {
-					var info = {};
-					info['id'] = param[0].id;
-					infoWindow =  window.open('waterLevel.html', "info");
-					
-					setTimeout(function(){ infoWindow.getInfo(info); }, 200);	
-				}
+                else {
+                    var info = {};
+                    info['id'] = param[0].id;
+                    infoWindow =  window.open('waterLevel.html', "info");
+                    
+                    setTimeout(function(){ infoWindow.getInfo(info); }, 200);   
+                }
                 break;
             case '10':
                 // 新視窗
-				var url = param[0].url;
-				window.open(url);
+                var url = param[0].url;
+                window.open(url);
                 break;
             case '11':
                 // 準眼
-				 if (allin1) {
+                 if (allin1) {
                     var id = param[0]["id"];
                     getEyeInfo(id, function (html){
                         if (videoWindow) {
@@ -289,78 +342,74 @@ $(document).ready(function() {
                         }
                     });
                 }
-				else {
-					var info = {};
-					info['id'] = param[0].id;
-					infoWindow =  window.open('eye.html', "info");
-					
-					setTimeout(function(){ infoWindow.getInfo(info); }, 200);	
-				}
-				
+                else {
+                    var info = {};
+                    info['id'] = param[0].id;
+                    infoWindow =  window.open('eyefalcon.html', "info");
+                    
+                    setTimeout(function(){ infoWindow.getInfo(info); }, 200);   
+                }
+                
                 break;
             case '12':
                 // 地震儀 earthquake
-				var info = {};
-				info['id'] = param[0].id;
-				infoWindow =  window.open('earthquake.html', "info");
-				
-				setTimeout(function(){ infoWindow.getInfo(info); }, 200);
+                var info = {};
+                info['id'] = param[0].id;
+                infoWindow =  window.open('earthquake.html', "info");
+                
+                setTimeout(function(){ infoWindow.getInfo(info); }, 200);
                 break;
             case '13':
                 // 量水堰 weir
-				var info = {};
-				info['id'] = param[0].id;
-				infoWindow =  window.open('weir.html', "info");
-				
-				setTimeout(function(){ infoWindow.getInfo(info); }, 200);
+                var info = {};
+                info['id'] = param[0].id;
+                infoWindow =  window.open('weir.html', "info");
+                
+                setTimeout(function(){ infoWindow.getInfo(info); }, 200);
                 break;
             case '14': 
                 // 地下水井 groundWater
-				var info = {};
-				info['id'] = param[0].id;
-				infoWindow =  window.open('groundWater.html', "info");
-				
-				setTimeout(function(){ infoWindow.getInfo(info); }, 200);
+                var info = {};
+                info['id'] = param[0].id;
+                infoWindow =  window.open('groundWater.html', "info");
+                
+                setTimeout(function(){ infoWindow.getInfo(info); }, 200);
                 break;
             default:
                 break;
         }
     }
 
-	function getTableHtml(data, name, className) {
+    function getTableHtml(data, name, className) {
         var html = "";
         html += "<table class='table table-striped table-bordered'><tr><td style='font-weight:bold;'>地點</td>";
         for (var i = 0; i < data.length; i++) {
             html += "<td>" + data[i].name + "</td>";
         }
-		html += "<td>最後更新</td>";
         html += "</tr>" + "<tbody><tr class='" + className +"'>" + "<td style='font-weight:bold;'>" + name + "</td>";
         for (var j = 0; j < data.length; j++) {
             html += "<td>" + data[j].value + "</td>";
         }
-		html += "<td>" + data[0].lastTime + "</td>";
         html += "</tr></tbody></table>";
 
         return html;
     }
-	
+
     function getEyeTableHtml(data, name, className) {
         var html = "";
         html += "<table class='table table-striped table-bordered'><tr><td style='font-weight:bold;'>地點</td>";
         for (var i = 0; i < data.length; i++) {
             html += "<td>" + data[i].name + "</td>";
         }
-		html += "<td>最後更新</td>";
         html += "</tr>" + "<tbody><tr class='" + className +"'>" + "<td style='font-weight:bold;'>" + '市電電壓' + "</td>";
         for (var j = 0; j < data.length; j++) {
             html += "<td>" + data[j].di + "</td>";
         }
-		html += "<td>" + data[0].lastTime + "</td>";
+        
         html += "</tr>" + "<tr class='" + className +"'>" + "<td style='font-weight:bold;'>" + 'UPS輸出電壓' + "</td>";
         for (var k = 0; k < data.length; k++) {
             html += "<td>" + data[k].do_out + "</td>";
         }
-		html += "<td>" + data[0].lastTime + "</td>";
         html += "</tr></tbody></table>";
 
         return html;
@@ -379,12 +428,12 @@ $(document).ready(function() {
             if (camList[i]["brand"] === "axis") {
                 hasAxis = true;
                 html += "<OBJECT ID='MyActiveX" + i + "' WIDTH=" + videoWidth + " HEIGHT=" + videoHeight + " CLASSID='CLSID:745395C8-D0E1-4227-8586-624CA9A10A8D' " +
-                        "codebase='AMC.cab'>" + "<param name='AutoStart' value='0'>" + "<param name='UIMode' value='ptz-absolute'>" +
+                        "codebase='AMC.cab' border='1'>" + "<param name='AutoStart' value='0'>" + "<param name='UIMode' value='ptz-absolute'>" +
                         "<param name='MediaType' value='h264'>" + "<param name='VideoRenderer' value='65536'>" +
                         "<param name='NetworkTimeout' value='5000'>" + "</OBJECT>";
             } else if (camList[i]["brand"] === "samsung"){
                 html += "<OBJECT ID='MyActiveX" + i + "' WIDTH=" + videoWidth + " HEIGHT=" + videoHeight + " CLASSID='CLSID:36299202-09EF-4ABF-ADB9-47C599DBE778' " +
-                        "codebase='myactivex.cab#version=1,0,0,1'></OBJECT>";
+                        "codebase='myactivex.cab#version=1,0,0,1' border='1'></OBJECT>";
             }
 
             html += "</div>";
@@ -490,11 +539,10 @@ $(document).ready(function() {
                 } 
                 else if (devMenu[i].content[j].icon === '5') {
                     htmlContent += "<li><img src='image/dome.png' class='domeIcon'/>";
-                }
-                else {
+                } else {
                     htmlContent += "<li><img src='image/utility.png' class='utilityIcon'/>";
                 }
-
+                
                 if (devMenu[i].content[j].multi === '1') {
                     htmlContent += "<span>" + devMenu[i].content[j].name + "</span>";
                     // layer 4
@@ -505,11 +553,9 @@ $(document).ready(function() {
                         }
                         else if (devMenu[i].content[j].content[k].icon === '3') {
                             htmlContent += "<li><img src='image/camera.png' class='cameraIcon'/>";
-                        }
-                        else if (devMenu[i].content[j].content[k].icon === '5') {
+                        } else if (devMenu[i].content[j].content[k].icon === '5') {
                             htmlContent += "<li><img src='image/dome.png' class='domeIcon'/>";
-                        } 
-                        else {
+                        } else {
                             htmlContent += "<li><img src='image/utility.png' class='utilityIcon'/>";
                         }
                         htmlContent += "<span class='devAction'id='dev-" + i + "-" + j + "-" + k + "'>" + devMenu[i].content[j].content[k].name + "</span></li>"
@@ -556,9 +602,9 @@ $(document).ready(function() {
                 j = parseInt(tmp[2], 10);
                 if (tmp.length === 4) {
                     k = parseInt(tmp[3], 10);
-                    handleAction(devMenu[i].content[j].content[k].act, devMenu[i].content[j].content[k].param);
+                    handleAction(devMenu[i].content[j].content[k].act, devMenu[i].content[j].content[k].param, false);
                 } else if (tmp.length === 3) {
-                    handleAction(devMenu[i].content[j].act, devMenu[i].content[j].param);
+                    handleAction(devMenu[i].content[j].act, devMenu[i].content[j].param, false);
                 }
             }
         });
@@ -605,11 +651,18 @@ $(document).ready(function() {
 
             var menuID;
             var menuIDSelect;
-            $("#siteMenu").append(mapPoints[index].name+ "<br>");
+            var html = "";
+
+            html += mapPoints[index].name + "<br><div>";
             for (var j = 0; j < mapPoints[index].content.length; j++) {
                 menuID = "menu" + j;
-                $("#siteMenu").append("<span id='" + menuID + "'>" + mapPoints[index].content[j].name + "</span><br>");
-                menuIDSelect = "#" + menuID;
+                html += "<span id='" + menuID + "'>" + mapPoints[index].content[j].name + "</span><br>";
+            }
+            html += "</div>";
+            $("#siteMenu").append(html);
+
+            for (var j = 0; j < mapPoints[index].content.length; j++) {
+                menuIDSelect = "#menu" + j;
 
                 $(menuIDSelect).bind("click", { act: mapPoints[index].content[j].actList }, function(event) {
                     handleActionList(event.data.act);
@@ -664,13 +717,13 @@ $(document).ready(function() {
     	}
     }
 
-	function getAccountInfo(data) {
-    	if (data) {
-    		account = data.account;
-    	}
+    function getAccountInfo(data) {
+        if (data) {
+            account = data.account;
+        }
     }
 
-	    function getDevInfo(data) {
+    function getDevInfo(data) {
         if (data) {
             nvrList = data.nvr;
             cameraList = data.camera;
@@ -683,9 +736,35 @@ $(document).ready(function() {
             type: "GET",
             data: "",
             success: function(data) {
+                console.log("load json: " + url + " success!");
                 callback(data);
             }
         })
+    }
+
+    function setCookie(c_name, value, expiredays)
+    {
+        var exdate = new Date();
+        exdate.setDate(exdate.getDate() + expiredays);
+        document.cookie = c_name + "=" + escape(value) +
+        ((expiredays == null) ? "" : ";expires=" + exdate.toGMTString());
+    }
+
+    function getCookie(c_name)
+    {
+        if (document.cookie.length > 0)
+        {
+            c_start=document.cookie.indexOf(c_name + "=");
+            if (c_start != -1)
+            { 
+                c_start = c_start + c_name.length + 1; 
+                c_end = document.cookie.indexOf(";",c_start);
+                if (c_end == -1) 
+                    c_end = document.cookie.length;
+                return unescape(document.cookie.substring(c_start, c_end));
+            } 
+        }
+        return "";
     }
 
     function padLeft(str) {
